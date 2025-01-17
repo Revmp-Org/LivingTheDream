e167712ffa0ade91a1520d6a1c36b25dfc5c26e1
caseymackrell <116777676+caseymackrell@users.noreply.github.com>
2025-01-17 12:13:06 -0500

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: { Component: React.ComponentType<any>; pageProps: any }) {
   return (
     <Layout>
       <Component {...pageProps} />
