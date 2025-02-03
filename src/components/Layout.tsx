9f85a014e457171533abce636b8b12a055d4ae8d
cmack912 <casey@revampedu.net>
2025-02-03 12:58:25 -0500

    update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -21,13 +21,14 @@ const Layout = ({ children }: LayoutProps) => {
                 <meta name="robots" content="index, follow" />
                 <meta property="og:type" content="website" />
 
-                {/* Open Graph Image for Social Previews */}
+                {/* Force Browsers to Use the PNG Instead of ICO */}
+                <link rel="icon" type="image/png" href="/logo-small.png" sizes="512x512" />
+                <link rel="apple-touch-icon" href="/logo-small.png" />
+
+                {/* Open Graph Image for Social Sharing */}
                 <meta property="og:image" content="/logo-small.png" />
                 <meta property="og:image:width" content="1200" />
                 <meta property="og:image:height" content="630" />
-
-                {/* Favicon */}
-                <link rel="icon" href="/logo-small.png" />
 
                 {/* Theme Color */}
                 <meta name="theme-color" content={themeColor} />
